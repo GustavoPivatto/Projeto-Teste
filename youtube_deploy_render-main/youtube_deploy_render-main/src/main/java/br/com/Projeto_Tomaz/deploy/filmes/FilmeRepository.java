6e75033102719acb80ead34ddfc7b032da0d83ba
GustavoPivatto <66446240+GustavoPivatto@users.noreply.github.com>
2024-03-25 10:12:56 -0300

    Delete youtube_deploy_render-main/youtube_deploy_render-main directory

--- youtube_deploy_render-main/youtube_deploy_render-main/src/main/java/br/com/Projeto_Tomaz/deploy/filmes/FilmeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.Projeto_Tomaz.deploy.filmes;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilmeRepository extends JpaRepository<Filme, UUID> {
-
-}
