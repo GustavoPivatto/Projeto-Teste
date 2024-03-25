f8f228dac9fa20f996bb1441ff6e887bf1b228eb
GustavoPivatto <66446240+GustavoPivatto@users.noreply.github.com>
2024-03-25 10:12:41 -0300

    Add files via upload

---
+++ src/main/java/br/com/Projeto_Tomaz/deploy/DeployRender.java
@@ -0,0 +1,13 @@
+package br.com.Projeto_Tomaz.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployRender {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployRender.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/Projeto_Tomaz/deploy/filmes/FilmeRepository.java
@@ -0,0 +1,9 @@
+package br.com.Projeto_Tomaz.deploy.filmes;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, UUID> {
+
+}
